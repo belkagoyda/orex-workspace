814d38d6359a27f90069b2e54f2cdccd948bee7e
belkagoyda <demozorr@yandex.ru>
2025-08-13 01:45:16 +0700

    обновил документацию, наметил структуру сайта

--- orex.py
+++ orex.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template#, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 orex = Flask(__name__)
 orex.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blod.db'
 
 @orex.route('/orex-ws')
-def index():
-    return "Слава белкам!"
+def base():
+    return "Выбор таблиц"
 
+@orex.route('/orex-ws/table')
+def table():
+        return "Таблица"
 if __name__ == "__main__":
     orex.run(debug=True)
     
